377cef977ae20201ce1208b987e6e356c1105452
RajpalPawar22 <rajpalpawar777@gmail.com>
2026-07-11 20:19:32 +0530

    deleted a file

--- test_pdf.rs
+++
@@ -1,9 +0,0 @@
-use pdf_extract;
-
-fn main() {
-    let path = "/home/rajpalsinghpanwar/Crumbs/data/test_files/IV Sem syllabus IT.pdf";
-    match pdf_extract::extract_text(path) {
-        Ok(text) => println!("Success: {} chars", text.len()),
-        Err(e) => println!("Error: {}", e),
-    }
-}
